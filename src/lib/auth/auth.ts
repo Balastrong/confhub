e371e7aa546f5f70054319a621742efaae40b5c7
Leonardo Montini <lion.48m@gmail.com>
2025-07-01 18:49:30 +0200

    bump deps

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import { db } from "../db"
-import { reactStartCookies } from "better-auth/react-start"
+// import { reactStartCookies } from "better-auth/react-start"
 import * as schema from "../db/schema"
 
 export const auth = betterAuth({
@@ -17,5 +17,5 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  plugins: [reactStartCookies()],
+  // plugins: [reactStartCookies()],
 })
